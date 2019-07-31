c6f4f217d5ec47cfcc335c45ce0f4f719dd02608
Joel Gaeta <joelsgaeta@gmail.com>
2019-07-31 16:03:20 -0400

    Updates

---
+++ precache-manifest.7fba80f7e8533802b9f26c63f580c0fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d203faa14ce3eabfd3328c826f06b5c9",
+    "url": "/react-cartoons/index.html"
+  },
+  {
+    "revision": "3fa3fcb1902c41484650",
+    "url": "/react-cartoons/static/css/main.95cb9167.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-cartoons/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3fa3fcb1902c41484650",
+    "url": "/react-cartoons/static/js/main.084e7a7a.chunk.js"
+  },
+  {
+    "revision": "4f53c22548f2dcb6233e",
+    "url": "/react-cartoons/static/js/runtime~main.f98b9495.js"
+  }
+]);
